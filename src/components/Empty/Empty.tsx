4ad1a891d6032f19cea9d0921650913119bbcc34
Taras Baran <baranprivat22@gmail.com>
2023-05-10 21:34:18 +0200

    created some warning components

---
+++ src/components/Empty/Empty.tsx
@@ -0,0 +1,10 @@
+import './Empty.scss';
+import { CiStickyNote } from 'react-icons/ci'
+
+const Empty = () => (
+  <div>
+    <CiStickyNote className="empty" size={30} />
+  </div>
+);
+
+export default Empty;
---
+++ src/components/Tip/Tip.tsx
@@ -0,0 +1,11 @@
+import { AiOutlinePlus } from 'react-icons/ai'
+import './Tip.scss';
+
+const Tip = () => (
+  <div className="tip">
+    <p className="tip__text">Please enter</p>
+    <AiOutlinePlus size={30} />
+  </div>
+);
+
+export default Tip;
